1a5b30be530a7b459bed619194ba6da67171287d
michelelozada <michelelozada1@gmail.com>
2022-07-19 17:43:25 -0300

    Added content

--- files/07-Operadores-de-incremento-e-decremento.php
+++ files/07-Operadores-de-incremento-e-decremento.php
@@ -13,33 +13,40 @@
 1.1 - Incrementa a variável em 1 e a retorna imediatamente (++$a)
 $a = 8;
 $b = ++$a;
-echo $b; # Retorna: 9
+echo $a;  # Retorna: 9
+echo $b; # 9
 $b += 5;
 echo $b; #14
-echo $a;  #9
 
 // Pós-incremento
 1.2 - Retorna a variável primeiro e depois a incrementa em 1 ($a++)
 $a = 8;
 $b = $a++;
-echo $b; # Retorna: 8
+echo $a;  # Retorna: 9
+echo $b; # 8
 $b += 5;
 echo $b; # 13
-echo $a;  # 9
 
 
 //2 - Operadores de decremento
 // Pré-decremento
 2.1 - Decrementa a variável em 1 e a retorna imediatamente  (--$a)
-$a = 10;
-echo '1ª impressão: ' . --$a; # 1ª impressão: 9
-echo '2ª impressão: ' . $a; # 2ª impressão: 9
+$a = 8;
+$b = --$a;
+echo $a; # Retorna: 7
+echo $b; # 7
+$b -= 5;
+echo $b; #2
 
 // Pós-decremento
 2.2 - Retorna a variável primeiro e depois a decrementa em 1 ($a--)
-$a = 10;
-echo '1ª impressão: ' . $a--; # 1ª impressão: 10
-echo '2ª impressão: ' . $a; # 2ª impressão: 9
+$a = 8;
+$b = $a--;
+echo $a; # Retorna: 7
+echo $b; # 8
+$b -= 5;
+echo $b; # 3
+
 
 
 // 3 - Incrementando letras
