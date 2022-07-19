c54db9d18e20fdf349b1b70152f96af6acbe9a8c
michelelozada <michelelozada1@gmail.com>
2022-07-19 17:37:01 -0300

    Added content

--- files/07-Operadores-de-incremento-e-decremento.php
+++ files/07-Operadores-de-incremento-e-decremento.php
@@ -8,27 +8,44 @@
 <?php
 
 //1 - Operadores de incremento
-// Pós incremento
-1.1 - Retorna a variável primeiro e depois a incrementa em 1 ($a++)
-$a = 10;
-echo '1ª impressão:' . $a++; # Retorna: 10
-echo '2ª impressão:' . $a; # 11
 
+// Pré-incremento
+1.1 - Incrementa a variável em 1 e a retorna imediatamente (++$a)
+$a = 8;
+$b = ++$a;
+echo $b; # Retorna: 9
+$b += 5;
+echo $b; #14
+echo $a;  #9
 
-// Pré incremento
-1.2 - Incrementa a variável em 1 e depois a retorna (++$a)
-$a = 10;
-echo '1ª impressão:' . ++$a; # Retorna: 11
-echo '2ª impressão:' . $a; # 11
+// Pós-incremento
+1.2 - Retorna a variável primeiro e depois a incrementa em 1 ($a++)
+$a = 8;
+$b = $a++;
+echo $b; # Retorna: 8
+$b += 5;
+echo $b; # 13
+echo $a;  # 9
 
 
 //2 - Operadores de decremento
-// Pós decremento
+// Pré-decremento
+2.1 - Decrementa a variável em 1 e a retorna imediatamente  (--$a)
+$a = 10;
+echo '1ª impressão: ' . --$a; # 1ª impressão: 9
+echo '2ª impressão: ' . $a; # 2ª impressão: 9
+
+// Pós-decremento
 2.2 - Retorna a variável primeiro e depois a decrementa em 1 ($a--)
 $a = 10;
-echo '1ª impressão:' . $a--; # Retorna: 10
-echo '2ª impressão:' . $a; # 9
+echo '1ª impressão: ' . $a--; # 1ª impressão: 10
+echo '2ª impressão: ' . $a; # 2ª impressão: 9
 
-2.2 - Retorna a variável e depois decrementa em 1 (--$a)
-echo '1ª impressão:' . --$a; # Retorna: 9
-echo '2ª impressão:' . $a; # 9
+
+// 3 - Incrementando letras
+$x = "a"; 
+echo $x++; # Retorna: a
+echo $x++; # b
+echo $x++; # c
+echo $x++; # d
+echo $x++; # e
